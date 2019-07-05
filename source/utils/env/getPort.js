a661095af8d5124e4ec188c020bb57d57da63eef
Andrii <andrey.filenochko@gmail.com>
2019-07-05 22:58:55 +0300

    Update getPort.js

--- source/utils/env/getPort.js
+++ source/utils/env/getPort.js
@@ -8,7 +8,7 @@ export const getPort = () => {
     const isValid = /^[3-9]{1}[0-9]{3}$/.test(PORT);
 
     if (!isValid) {
-        throw new Error(
+        throw new ValidationError(
             'Environment variable PORT should a number and be between 3000 and 9999',
         );
     }
