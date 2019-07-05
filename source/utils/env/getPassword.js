74c37d734458c55d1143f6db8d4597f812767737
Andrii <andrey.filenochko@gmail.com>
2019-07-05 22:59:19 +0300

    Update getPassword.js

--- source/utils/env/getPassword.js
+++ source/utils/env/getPassword.js
@@ -2,7 +2,7 @@ export const getPassword = () => {
     const { PASSWORD } = process.env;
 
     if (!PASSWORD) {
-        throw new Error('Environment variable PASSWORD should be specified');
+        throw new ValidationError('Environment variable PASSWORD should be specified');
     }
 
     return PASSWORD;
